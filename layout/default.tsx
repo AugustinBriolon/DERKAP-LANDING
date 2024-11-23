5dd9307cfee47daf1d2c1409db0a6cac2a9cbf7e
Augustin Briolon <augustin.briolon@gmail.com>
2024-11-23 17:03:06 +0100

    :dizzy: add anim on lines and hero element

--- layout/default.tsx
+++ layout/default.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { champFont } from '@/public/font/font';
 
 const Layout = ({ children }: { children: ReactNode }) => {
 
   return (
-    <div className='max-w-screen-2xl overflow-hidden mx-auto'>
+    <div className={`max-w-screen-2xl overflow-hidden mx-auto ${champFont.className}`}>
       <Header />
       {children}
       <Footer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,6 @@ export default function Home() {
 
   const timelineAnimation = () => {
     gsap.timeline().fromTo(
-      ".hero-title",
-      {
-        yPercent: 100,
-      },
-      {
-        yPercent: 0,
-        duration: .8,
-        ease: "power2.out",
-      }
-    ).fromTo(
       ".hero-img",
       {
         y: 100,
@@ -25,8 +15,7 @@ export default function Home() {
         y: 0,
         duration: .8,
         ease: "power2.out",
-      },
-      "<"
+      }
     )
   }
 
@@ -38,15 +27,14 @@ export default function Home() {
     <>
       <div className="min-h-screen w-screen">
         <div id="hero" className="h-screen py-32 flex flex-col items-center justify-start gap-8 px-2 md:px-4">
+          <h1 className="hero-title w-full text-5xl text-center text-pretty">Tape toi des barres en <span className="text-purple">défiant</span> tes potes</h1>
           <div className="overflow-hidden">
-            <h1 className="hero-title w-full text-5xl text-center text-pretty">Tape toi des barres en <span className="text-purple">défiant</span> tes potes</h1>
-          </div>
-          <div className="overflow-hidden">
-            <Image src="/images/img-hero.webp" alt="Trois téléphones montrant l'application Derkap" width={4188} height={3112} className="hero-img h-full w-auto object-contain" />
+            <Image src="/images/img-hero.webp" alt="Trois téléphones montrant l'application Derkap" width={4188} height={3112} className="hero-img h-full w-auto max-w-[1000px] object-contain" />
           </div>
         </div>
-        <Lines textOne="Relève les Pdéfis" textTwo="DERKAP" black />
+        <Lines textOne="Relève les Défis" textTwo="DERKAP" black />
         <div className="h-screen"></div>
+        <Lines textOne="Relève les Défis" textTwo="DERKAP" purple />
       </div>
     </>
   );
